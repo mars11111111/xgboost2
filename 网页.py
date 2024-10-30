3031ce51e5a21218d1c67338a27343c285e25915
mars11111111 <1638948183@qq.com>
2024-10-30 21:57:36 +0800

    Update 网页.py

--- 网页.py
+++ 网页.py
@@ -178,9 +178,9 @@ def predict():
 
         # Calculate SHAP values and display force plot
         explainer = shap.TreeExplainer(model)
-        shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
+        shap_values = explainer.shap_values(data_df)
 
-        shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
+        shap.force_plot(explainer.expected_value, shap_values[0], data_df, matplotlib=True)
         plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
  
         st.image("shap_force_plot.png")
